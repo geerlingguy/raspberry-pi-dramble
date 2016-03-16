7c2c1e30076bdff00165ad1e0cf80c49e844cf56
Jeff Geerling <geerlingguy@mac.com>
2016-03-15 23:06:27 -0500

    Some updates to the benchmarking script.

--- setup/benchmarks/microsd-benchmarks.sh
+++ setup/benchmarks/microsd-benchmarks.sh
@@ -2,16 +2,15 @@
 
 # Raspberry Pi microSD card benchmark script.
 #
-# Just a little script I use to automate the running and reporting of benchmarks
-# I compile on this page: http://www.pidramble.com/wiki/benchmarks/microsd-cards
+# A script I use to automate the running and reporting of benchmarks I compile
+# for: http://www.pidramble.com/wiki/benchmarks/microsd-cards
 #
 # Usage:
 #   # Run it locally.
 #   $ sudo ./microsd-benchmarks.sh
 #
 #   # Run it straight from GitHub.
-#   $ curl https://raw.githubusercontent.com/geerlingguy/raspberry-pi-dramble/\
-#     master/setup/benchmarks/microsd-benchmarks.sh | sudo bash
+#   $ curl https://raw.githubusercontent.com/geerlingguy/raspberry-pi-dramble/master/setup/benchmarks/microsd-benchmarks.sh | sudo bash
 #
 # Author: Jeff Geerling, 2016
 
@@ -19,17 +18,32 @@
 IOZONE_INSTALL_PATH=/home/pi
 IOZONE_VERSION=iozone3_434
 
-cd $INSTALL_PATH
+cd $IOZONE_INSTALL_PATH
 
 # Install apt dependencies.
-apt-get install -y hdparm curl
+if [ ! `which hdparm` ]; then
+  printf "Installing apt dependencies...\n"
+  apt-get install -y hdparm curl
+  printf "Install complete!\n\n"
+fi
 
 # Download and build iozone.
-curl "http://www.iozone.org/src/current/$IOZONE_VERSION.tar" | tar -x
-cd $IOZONE_VERSION/src/current
-make --quiet linux-arm
+if [ ! -f $IOZONE_INSTALL_PATH/$IOZONE_VERSION/src/current/iozone ]; then
+  printf "Installing iozone...\n"
+  curl "http://www.iozone.org/src/current/$IOZONE_VERSION.tar" | tar -x
+  cd $IOZONE_VERSION/src/current
+  make --quiet linux-arm
+  printf "Install complete!\n\n"
+fi
 
 # Run benchmarks.
+printf "Running hdparm test...\n"
 hdparm -t /dev/mmcblk0
+
+printf "Running dd test...\n"
 dd if=/dev/zero of=/home/pi/test bs=8k count=50k conv=fsync; rm -f /home/pi/test
+
+printf "Running iozone test...\n"
 ./iozone -e -I -a -s 100M -r 4k -i 0 -i 1 -i 2
+
+printf "microSD card benchmark complete!\n"
