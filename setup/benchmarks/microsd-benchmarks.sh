b7a0df40dfd289984d96676965a7e4751379e74b
Jeff Geerling <geerlingguy@mac.com>
2016-03-15 23:23:00 -0500

    Add clock output to microSD benchmark.

--- setup/benchmarks/microsd-benchmarks.sh
+++ setup/benchmarks/microsd-benchmarks.sh
@@ -12,7 +12,16 @@
 #   # Run it straight from GitHub.
 #   $ curl https://raw.githubusercontent.com/geerlingguy/raspberry-pi-dramble/master/setup/benchmarks/microsd-benchmarks.sh | sudo bash
 #
+# Another good benchmark:
+#   $ curl http://www.nmacleod.com/public/sdbench.sh | sudo bash
+#
 # Author: Jeff Geerling, 2016
+
+printf "Raspberry Pi Dramble microSD benchmarks\n"
+
+CLOCK="$(grep "actual clock" /sys/kernel/debug/mmc0/ios 2>/dev/null | awk '{printf("%0.3f MHz", $3/1000000)}')"
+echo "microSD clock: $CLOCK"
+printf "\n"
 
 # Variables.
 IOZONE_INSTALL_PATH=/home/pi
@@ -39,11 +48,14 @@ fi
 # Run benchmarks.
 printf "Running hdparm test...\n"
 hdparm -t /dev/mmcblk0
+printf "\n\n"
 
 printf "Running dd test...\n"
 dd if=/dev/zero of=/home/pi/test bs=8k count=50k conv=fsync; rm -f /home/pi/test
+printf "\n\n"
 
 printf "Running iozone test...\n"
 ./iozone -e -I -a -s 100M -r 4k -i 0 -i 1 -i 2
+printf "\n\n"
 
 printf "microSD card benchmark complete!\n"
